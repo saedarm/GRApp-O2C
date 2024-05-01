6da0af1fac6ebed529827d356a3e1fc9fddb8b5b
saedarm <searmentrout@gmail.com>
2024-05-01 08:43:35 -0400

    modifiedresults images

--- public/countdown.js
+++ public/countdown.js
@@ -1,23 +1,24 @@
 // public/countdown.js
-// Countdown timer
-var countdownElement = document.getElementById('timer');
-var countdownInterval = null;
+document.addEventListener('DOMContentLoaded', function () {
+    var countdownElement = document.getElementById('timer');
+    var countdownInterval = null;
 
-// Function to start countdown
-function startCountdown() {
-    var countdown = 10; // Countdown time in seconds
-    countdownInterval = setInterval(function () {
-        countdown--;
-        countdownElement.textContent = countdown;
-        if (countdown <= 0) {
-            clearInterval(countdownInterval);
-            revealImage();
-        }
-    }, 1000);
-}
+    function startCountdown() {
+        var countdown = 10; // Countdown time in seconds
+        countdownInterval = setInterval(function () {
+            countdown--;
+            countdownElement.textContent = countdown;
+            if (countdown <= 0) {
+                clearInterval(countdownInterval);
+                revealImage();
+            }
+        }, 1000);
+    }
 
-// Function to reveal the image
-function revealImage() {
-    document.getElementById('countdown').style.display = 'none';
-    document.getElementById('reveal-image').innerHTML = '<img src="<%= currentImage %>" alt="Reveal Image" width="400"><p id="secret-revealed">Secret Revealed: <%= secret %></p>';
-}
+    function revealImage() {
+        document.getElementById('countdown').style.display = 'none';
+        document.getElementById('reveal-image').innerHTML = '<img src="<%= currentImage %>" alt="Reveal Image" width="400"><p id="secret-revealed">Secret Revealed: <%= secret %></p>';
+    }
+
+    startCountdown();
+});
